67ca36b94353901b2805d490f8ad5f109214753c
Sumul Shah <sumul@sumulshah.com>
2017-04-27 15:23:48 -0700

    Added preview template that supports client-side rendering.

--- fractal_react/__components.js
+++ fractal_react/__components.js
@@ -1,5 +1,6 @@
 /* NOTE: This file is auto-generated while Fractal is running! */
 
 module.exports = {
+  preview: require("../components/_preview.jsx"),
   sample: require("../components/sample/sample.jsx"),
 };
