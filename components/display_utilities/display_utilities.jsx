e58f7786c2ab85857c689df8c6a8192627659d1b
Sumul Shah <sumul@sumulshah.com>
2017-05-09 14:05:14 -0700

    Demo component formatting.

--- components/display_utilities/display_utilities.jsx
+++ components/display_utilities/display_utilities.jsx
@@ -3,12 +3,22 @@ import React from 'react';
 function DisplayUtilities(props) {
   return (
     <div>
-      <div className="d-none">Hidden at xs size and up</div>
-      <div className="d-none-sm">Hidden at sm size and up</div>
-      <div className="d-none-md">Hidden at md size and up</div>
-      <div className="d-none-lg">Hidden at lg size and up</div>
-      <div className="d-none-xl">Hidden at xl size and up</div>
-      <br/>
+      <p><code className="text-color-secondary">.d-none</code></p>
+      <div className="d-none">I'm hidden at xs size and up</div>
+      <hr/>
+      <p><code className="text-color-secondary">.d-none-sm</code></p>
+      <div className="d-none-sm">I'm hidden at sm size and up</div>
+      <hr/>
+      <p><code className="text-color-secondary">.d-none-md</code></p>
+      <div className="d-none-md">I'm hidden at md size and up</div>
+      <hr/>
+      <p><code className="text-color-secondary">.d-none-lg</code></p>
+      <div className="d-none-lg">I'm hidden at lg size and up</div>
+      <hr/>
+      <p><code className="text-color-secondary">.d-none-xl</code></p>
+      <div className="d-none-xl">I'm hidden at xl size and up</div>
+      <hr/>
+      <p><code className="text-color-secondary">.d-flex-sm</code></p>
       <div className="d-flex-sm">
         <div>Column 1</div>
         <div>Column 2</div>
