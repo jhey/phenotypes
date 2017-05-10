c0705c56d0e865a64abbd21a9e04d17aa007b1d6
Sumul Shah <sumul@sumulshah.com>
2017-05-10 11:17:04 -0700

    Renamed CSS utility components.

--- fractal_react/__components.js
+++ fractal_react/__components.js
@@ -1,8 +1,8 @@
 /* NOTE: This file is auto-generated while Fractal is running! */
 
 module.exports = {
-  css-display-utilities: require("../components/display_utilities/display_utilities.jsx"),
-  css-text-utilities: require("../components/text_utilities/text_utilities.jsx"),
+  css-display-utilities: require("../components/css_display_utilities/css_display_utilities.jsx"),
+  css-text-utilities: require("../components/css_text_utilities/css_text_utilities.jsx"),
   preview: require("../components/_preview.jsx"),
   sample: require("../components/sample/sample.jsx"),
 };
