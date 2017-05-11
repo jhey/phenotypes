f5f9e856df247a81b67da7a7afb1a941fd262781
Sumul Shah <sumul@sumulshah.com>
2017-05-11 12:39:12 -0700

    Added sass variables to spacing guide appendix.

--- components/css_spacing_utilities/css_spacing_utilities.jsx
+++ components/css_spacing_utilities/css_spacing_utilities.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 function SpacingUtilities(props) {
   return (
     <div>
+      <p><code className="text-color-hint">.p3.p4-sm.p5-md.p6-lg.p7-xl.m3.m4-sm.m5-md.m6-lg.m7-xl</code></p>
       <h4 style={{ background: '#232323' }} className="text-color-reversed-primary p3 p4-sm p5-md p6-lg p7-xl m3 m4-sm m5-md m6-lg m7-xl h6 h5-sm h4-md h3-lg h2-xl">🚀 I'm going to space (resize me)</h4>
     </div>
   );
