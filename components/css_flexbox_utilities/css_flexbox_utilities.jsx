cb5918848b971d0163e8b301727b488db2344205
Sumul Shah <sumul@sumulshah.com>
2017-05-11 16:33:46 -0700

    Okay, docker is working again.

--- components/css_flexbox_utilities/css_flexbox_utilities.jsx
+++ components/css_flexbox_utilities/css_flexbox_utilities.jsx
@@ -1,58 +1,48 @@
 import React from 'react';
 
-// function Label(props) {
-//   var className = props.className || 'text-color-hint';
-//   return (
-//     <p><code className={className}>{props.children}</code></p>
-//   );
-// }
+function Label(props) {
+  var className = props.className || 'text-color-hint';
+  return (
+    <p><code className={className}>{props.children}</code></p>
+  );
+}
 
-// function Separator(props) {
-// 	return (
-// 		<hr className="my6" />
-// 	);
-// }
+function Separator(props) {
+	return (
+		<hr className="my6" />
+	);
+}
 
-// function Demo(props) {
-// 	const boxStyle = { background: '#eee' };
+function Demo(props) {
+	const boxStyle = { background: '#eee' };
 
-// 	const extraClassNames = props.extraClassNames || '';
-// 	const allClassNames = props.classNames + ' ' + extraClassNames;
+	const extraClassNames = props.extraClassNames || '';
+	const allClassNames = props.classNames + ' ' + extraClassNames;
 
-// 	return (
-// 		<div>
-// 			<Label>{ props.classNames.map(className => '.' + className + ' ') }</Label>
-// 			<div className={allClassNames} style={boxStyle}>{props.children}</div>
-// 		</div>
-// 	);
-// }
-
-// function FlexboxUtilities(props) {
-//   return (
-//     <div>
-//     	<h3>These display utilities are needed for flexbox</h3>
-//     	<p>Apply these display utilities to an element to create a flexbox container. This transforms direct children elements into flex items. Apply additional flex properties shown below to further customize.</p>
-// 	    <Demo classNames="d-flex" extraClassNames="p3">I'm a flexbox container!</Demo>
-//     	<Separator />
-//     	<Demo classNames="d-inline-flex" extraClassNames="p3">I'm an inline flexbox container!</Demo>
-
-//     	<h3 className="mt7">Direction</h3>
-//     	<Demo classNames="d-flex flex-column flex-row-sm" extraClassNames="p3">
-//     	hi
-//     	</Demo>
-
-//     </div>
-//   );
-// }
-
+	return (
+		<div>
+			<Label>{ props.classNames.split(' ').map(className => '.' + className + ' ') }</Label>
+			<div className={allClassNames} style={boxStyle}>{props.children}</div>
+		</div>
+	);
+}
 
 function FlexboxUtilities(props) {
   return (
     <div>
-    	wtf
+    	<h3>These display utilities are needed for flexbox</h3>
+    	<p>Apply these display utilities to an element to create a flexbox container. This transforms direct children elements into flex items. Apply additional flex properties shown below to further customize.</p>
+	    <Demo classNames="d-flex" extraClassNames="p3">I'm a flexbox container!</Demo>
+    	<Separator />
+    	<Demo classNames="d-inline-flex" extraClassNames="p3">I'm an inline flexbox container!</Demo>
+
+    	<h3 className="mt7">Direction</h3>
+    	<Demo classNames="d-flex flex-column flex-row-sm" extraClassNames="p3">
+    	hi
+    	</Demo>
+
     </div>
   );
 }
-
 
 module.exports = FlexboxUtilities;
