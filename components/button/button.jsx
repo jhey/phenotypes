ca483d7538d9610bf04e33b0ad476f0363c2194d
Laura Gelston <lauragelston@gmail.com>
2017-06-20 12:13:35 -0700

    Break up anchor style vc true buttons, optionally disable anchor style buttons

--- components/button/button.jsx
+++ components/button/button.jsx
@@ -1,57 +1,22 @@
 const React = require('react');
 const classes = require('classnames');
 
-const BUTTON_TYPES = {
-  button: 'button',
-  submit: 'submit',
-  reset: 'reset',
-};
-
-function validateType(type) {
-  if (BUTTON_TYPES[type]) {
-    return type;
-  }
-  throw new Error(`Unknown button type: ${type}`);
-}
-
+const AnchorButton = require('./anchor_button.jsx');
+const TrueButton = require('./true_button.jsx');
 
 function Button(props) {
-  const className = classes(props.className, 'Button', {
-    'Button--is-disabled': !!props.disabled,
-  });
+  const Component = props.href ? AnchorButton : TrueButton;
+  const isDisabled = !!props.disabled;
 
-  // If we have an `href` prop, render an anchor tag styled to look like a button:
-  if (props.href) {
-    return (
-      <a
-        className={className}
-        href={props.href}
-        onClick={props.onClick}
-        rel={props.rel}
-        role="button"
-        target={props.target}
-      >
-        {props.children}
-      </a>
-    );
-  }
-
-  // Otherwise, render a regular button element:
   return (
-    <button
-      className={className}
-      disabled={props.disabled}
-      form={props.form}
-      onClick={props.onClick}
-      type={validateType(props.type)}
-    >
-      {props.children}
-    </button>
+    <Component
+      {...props}
+      aria-disabled={isDisabled}
+      className={classes(props.className, 'Button', {
+        'Button--is-disabled': isDisabled,
+      })}
+    />
   );
 }
-
-Button.defaultProps = {
-  type: 'button',
-};
 
 module.exports = Button;
