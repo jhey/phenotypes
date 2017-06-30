dd981dfc794bccf106759601d6dfe3802db3c3af
Kevin Qi <kqi@amino.com>
2017-06-30 14:23:56 -0700

    tweak example

--- components/grid/grid.jsx
+++ components/grid/grid.jsx
@@ -5,19 +5,21 @@ function GridExamples() {
     <div className="grid-examples">
       <div className="container">
         <p>
-          Responsive! Add multiple classes with responsive suffixes to
+          <strong>Responsive:</strong> add multiple classes with responsive suffixes to
           change column widths depending on breakpoint.
         </p>
 
         <div className="row">
           <div className="col-4 col-2-md">.col-4.col-2-md</div>
-          <div className="col-4 col-8-md">.col-4.col-8-md</div>
+          <div className="col-4 col-8-md">
+            Resize me! I'm wider when the screen is wider.<br />.col-4.col-8-md
+          </div>
           <div className="col-4 col-2-md">.col-4.col-2-md</div>
         </div>
 
         <hr />
 
-        <p>Offsets!</p>
+        <p><strong>Offsets</strong></p>
 
         <div className="row">
           <div className="col-6 offset-3">.col-6.offset-3</div>
@@ -26,8 +28,9 @@ function GridExamples() {
         <hr />
 
         <p>
-          Customize gutters to your deepest desires by adding <code>.no-gutters</code>
-          to your <code>.row</code> class, and then using spacing helpers on the <code>.col-*</code> classes.
+          <strong>Custom gutter spacing</strong>:
+          customize gutter widths by adding <code>.no-gutters</code> to
+          your <code>.row</code> class, and then using spacing helpers on the <code>.col-*</code> classes.
         </p>
 
         <div className="row no-gutters">
