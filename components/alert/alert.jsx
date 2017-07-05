8ca2e08cab9f582d94d1744b09dfab4b732c1716
Kevin Qi <kqi@amino.com>
2017-07-05 15:33:29 -0700

    add alert styling and examples

--- components/alert/alert.jsx
+++ components/alert/alert.jsx
@@ -2,8 +2,22 @@ const React = require('react');
 
 function AlertExamples() {
   return (
-    <div className="Alert">
-      Hello, I'm alarming!
+    <div>
+      <div className="Alert Alert--success">
+        You did good.
+      </div>
+
+      <br />
+
+      <div className="Alert Alert--info">
+        I’m not very alarming but I get your attention.
+      </div>
+
+      <br />
+
+      <div className="Alert Alert--danger">
+        ...we’ve got a problem.
+      </div>
     </div>
   );
 }
