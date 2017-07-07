a42d00552d815c76c4128fb414c86447b65ac879
Sumul Shah <sumul@sumulshah.com>
2017-07-07 17:19:34 -0500

    Added unit tests.

--- components/checkbox/checkbox.spec.jsx
+++ components/checkbox/checkbox.spec.jsx
@@ -8,5 +8,45 @@ const sinon = require('sinon');
 const Checkbox = require('./checkbox');
 
 describe('Checkbox', function () {
+  it('should render input element', function () {
+    const wrapper = shallow(<Checkbox label="foo" />);
+    const input = wrapper.find('input');
 
+    expect(input).to.have.length(1);
+    const inputProps = input.props();
+    expect(inputProps.type).to.equal('checkbox');
+  });
+
+  it('should pass HTML attributes to input element and apply disabled class', function () {
+    const wrapper = shallow(
+      <Checkbox
+        className="className"
+        disabled="disabled"
+        id="id"
+        name="name"
+        value="value"
+        checked="checked"
+      />
+    );
+
+    const wrapperProps = wrapper.find('label').props();
+    expect(wrapperProps.className).to.equal('className Checkbox Checkbox--is-disabled');
+
+    const inputProps = wrapper.find('input').props();
+    expect(inputProps.className).to.equal('Checkbox__input');
+    expect(inputProps.disabled).to.equal('disabled');
+    expect(inputProps.id).to.equal('id');
+    expect(inputProps.name).to.equal('name');
+    expect(inputProps.value).to.equal('value');
+    expect(inputProps.checked).to.equal('checked');
+  });
+
+
+  it('should call onChange', function () {
+    const callMe = sinon.spy();
+    const wrapper = shallow(<Checkbox onChange={callMe} />);
+
+    wrapper.find('input').simulate('change');
+    expect(callMe.calledOnce).to.be.true;
+  });
 });
