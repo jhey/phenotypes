948e73f689236f833e27a5323229a0382f3140d5
Nick Dunkman <nickdunkman@gmail.com>
2017-07-18 16:13:40 -0700

    grid is a utility

--- fractal_react/__components.js
+++ fractal_react/__components.js
@@ -1,16 +1,16 @@
 /* NOTE: This file is auto-generated while Fractal is running! */
 
 module.exports = {
-  "display": require("../library/00-utilities/display/display.jsx"),
-  "flexbox": require("../library/00-utilities/flexbox/flexbox.jsx"),
-  "spacing": require("../library/00-utilities/spacing/spacing.jsx"),
-  "typography": require("../library/00-utilities/typography/typography.jsx"),
   "button": require("../library/components/button/button.jsx"),
   "checkbox": require("../library/components/checkbox/checkbox.jsx"),
   "form-group": require("../library/components/form_group/form_group.jsx"),
   "message": require("../library/components/message/message.jsx"),
   "tab": require("../library/components/tab/_tab.jsx"),
   "text-input": require("../library/components/text_input/text_input.jsx"),
-  "grid": require("../library/grid/grid.jsx"),
   "preview": require("../library/_preview.jsx"),
+  "display": require("../library/utilities/display/display.jsx"),
+  "flexbox": require("../library/utilities/flexbox/flexbox.jsx"),
+  "grid": require("../library/utilities/grid/grid.jsx"),
+  "spacing": require("../library/utilities/spacing/spacing.jsx"),
+  "typography": require("../library/utilities/typography/typography.jsx"),
 };
