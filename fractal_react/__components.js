0b164f213608f5cc5b66b23823d50eace1fa18ff
Nick Dunkman <nickdunkman@gmail.com>
2017-07-18 16:07:30 -0700

    move components into separate directory

--- fractal_react/__components.js
+++ fractal_react/__components.js
@@ -1,16 +1,16 @@
 /* NOTE: This file is auto-generated while Fractal is running! */
 
 module.exports = {
-  "button": require("../library/button/button.jsx"),
   "display": require("../library/00-utilities/display/display.jsx"),
   "flexbox": require("../library/00-utilities/flexbox/flexbox.jsx"),
   "spacing": require("../library/00-utilities/spacing/spacing.jsx"),
   "typography": require("../library/00-utilities/typography/typography.jsx"),
-  "checkbox": require("../library/checkbox/checkbox.jsx"),
-  "form-group": require("../library/form_group/form_group.jsx"),
+  "button": require("../library/components/button/button.jsx"),
+  "checkbox": require("../library/components/checkbox/checkbox.jsx"),
+  "form-group": require("../library/components/form_group/form_group.jsx"),
+  "message": require("../library/components/message/message.jsx"),
+  "tab": require("../library/components/tab/_tab.jsx"),
+  "text-input": require("../library/components/text_input/text_input.jsx"),
   "grid": require("../library/grid/grid.jsx"),
-  "message": require("../library/message/message.jsx"),
   "preview": require("../library/_preview.jsx"),
-  "tab": require("../library/tab/_tab.jsx"),
-  "text-input": require("../library/text_input/text_input.jsx"),
 };
