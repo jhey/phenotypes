2bf389bd2525c3b9e77256b1eb117396d0e7c074
NickDunkman <nickdunkman@gmail.com>
2017-10-24 12:24:34 -0700

    Remove Radio—is-disabled

--- library/components/radio/radio.jsx
+++ library/components/radio/radio.jsx
@@ -6,11 +6,7 @@ const classes = require('classnames');
 function Radio(props) {
   const { className, children, ...htmlProps } = props;
   return (
-    <label className={classes(
-      className,
-      'Radio',
-      { 'Radio--is-disabled': htmlProps.disabled }
-    )}>
+    <label className={classes(className, 'Radio')}>
       <input type="radio" className="Radio__input" { ...htmlProps } />
       <span className="Radio__indicator"></span>
       <span className="Radio__label">{ children }</span>
