fe738115b4ee1fdd95679ca2c628804fd49f8759
NickDunkman <nickdunkman@gmail.com>
2017-10-26 13:15:37 -0700

    Move this outside the class & rename so it looks nicer in React inspector

--- library/components/radio/radio_group.jsx
+++ library/components/radio/radio_group.jsx
@@ -1,39 +1,39 @@
 const React = require('react');
 const Radio = require('./radio.jsx');
 
+function ManagedRadio(radioProps) {
+  const groupProps = this.props;
+  const checked = radioProps.value === groupProps.value;
+  return (
+    <Radio
+      {...radioProps}
+      name={groupProps.name}
+      checked={checked}
+      disabled={groupProps.disabled || radioProps.disabled}
+      onChange={() => {
+        // Call the radio's own onChange if it has one.
+        radioProps.onChange && radioProps.onChange();
+
+        // The group's onChange callback is only called when the value actually changes.
+        if (groupProps.onChange) {
+          if (!checked || groupProps.clearable) {
+            const newValue = checked ? null : radioProps.value;
+            groupProps.onChange(newValue);
+          }
+        }
+      }}
+    />
+  );
+}
+
 class RadioGroup extends React.Component {
   constructor(props, context) {
     super(props, context);
-    this.renderManagedRadio = this.renderManagedRadio.bind(this);
-  }
-
-  renderManagedRadio(radioProps) {
-    const groupProps = this.props;
-    const checked = radioProps.value === groupProps.value;
-    return (
-      <Radio
-        {...radioProps}
-        name={groupProps.name}
-        checked={checked}
-        disabled={groupProps.disabled || radioProps.disabled}
-        onChange={() => {
-          // Call the radio's own onChange if it has one.
-          radioProps.onChange && radioProps.onChange();
-
-          // The group's onChange callback is only called when the value actually changes.
-          if (groupProps.onChange) {
-            if (!checked || groupProps.clearable) {
-              const newValue = checked ? null : radioProps.value;
-              groupProps.onChange(newValue);
-            }
-          }
-        }}
-      />
-    );
+    this.managedRadio = ManagedRadio.bind(this);
   }
 
   render() {
-    return this.props.render(this.renderManagedRadio);
+    return this.props.render(this.managedRadio);
   }
 }
 
