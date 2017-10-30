c7d72595f93ff9f56b6c2e44c3ae3d0863f37fce
Laura Gelston <lauragelston@gmail.com>
2017-10-30 13:56:20 -0700

    Add comments explaining extra modifiers

--- library/components/checkbox/checkbox.jsx
+++ library/components/checkbox/checkbox.jsx
@@ -4,7 +4,14 @@ import classes from 'classnames';
 function Checkbox(props) {
   const { className, label, ...htmlProps } = props;
   return (
-    <label className={classes(className, 'Checkbox', { 'Checkbox--is-disabled': htmlProps.disabled })}>
+    <label
+      className={classes(className, 'Checkbox', {
+        // Most of the styling for disabled checkboxes is tied to the :disabled input state --
+        // adding this modifier on the root element enables the `not-allowed` cursor for
+        // child elements with `pointer-events: none`
+        'Checkbox--is-disabled': htmlProps.disabled,
+      })}
+    >
       <input type="checkbox" className="Checkbox__input" {...htmlProps} />
       <span className="Checkbox__indicator" />
       <span className="Checkbox__label">{ props.label }</span>
