007d56a0096a28a9d9eb1c9604126db559e6c504
Ahmed Modan <ahmed.modan@gmail.com>
2020-06-17 14:56:41 -0700

    babelHelpers for both library and fractal server

--- configs/rollup.phenotypes-library.config.js
+++ configs/rollup.phenotypes-library.config.js
@@ -3,45 +3,53 @@ import path from 'path';
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-import {WORKSPACE_ROOT} from './constants.js';
+import { WORKSPACE_ROOT } from './constants.js';
 import packageJSON from '../package.json';
 
 const commonRollupConfig = {
-    input: path.resolve(WORKSPACE_ROOT, 'public_api.js'),
-
-    plugins: [
-        babel({
-            extensions: ['.js', '.jsx'],
-            babelHelpers: 'bundled'
-        }),
-        resolve(),
-        commonjs(),
-    ],
-    // do not bundle peer dependencies.
-    // these are dependencies that are required to run,
-    // however, these should be installed by the consuming
-    // application and not bundled with the library
-    external: [
-        ...Object.keys(packageJSON.peerDependencies)
-    ]
+  input: path.resolve(WORKSPACE_ROOT, 'public_api.js'),
+  plugins: [
+    babel({
+      extensions: ['.js', '.jsx'],
+      // for the library build we want babel helpers
+      // to not be included in the bundle.
+      // we want it to be the consuming application's
+      // responsibility to provide the @babel/runtime
+      // This is why we also override the babel preset
+      // here. The airbnb babel preset by default includes
+      // @babel/plugin-transform-runtime, which makes
+      // @babel/runtime a peer dependency of the application.
+      // This is what we want for the library but not for the
+      // fractal application.
+      babelHelpers: 'runtime',
+      presets: ['airbnb'],
+    }),
+    resolve(),
+    commonjs(),
+  ],
+  // do not bundle peer dependencies.
+  // these are dependencies that are required to run,
+  // however, these should be installed by the consuming
+  // application and not bundled with the library
+  external: [...Object.keys(packageJSON.peerDependencies)],
 };
 
 export default [
-    {
-        ...commonRollupConfig,
-        output: {
-            file: path.resolve(WORKSPACE_ROOT, packageJSON.module),
-            // for modern bundlers like webpack 2+ and rollup
-            // output esm format
-            format: 'esm'
-        },
+  {
+    ...commonRollupConfig,
+    output: {
+      file: path.resolve(WORKSPACE_ROOT, packageJSON.module),
+      // for modern bundlers like webpack 2+ and rollup
+      // output esm format
+      format: 'esm',
     },
-    {
-        ...commonRollupConfig,
-        output: {
-            file: path.resolve(WORKSPACE_ROOT, packageJSON.main),
-            // output commonjs format for older workflows
-            format: 'cjs'
-        },
+  },
+  {
+    ...commonRollupConfig,
+    output: {
+      file: path.resolve(WORKSPACE_ROOT, packageJSON.main),
+      // output commonjs format for older workflows
+      format: 'cjs',
     },
+  },
 ];
